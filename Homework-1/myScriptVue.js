80f5a0c75ab12dc23ee6f7b43c41238531679739
Arkhipov Mikhail <arhipovma@yandex.ru>
2020-11-30 19:17:30 +0500

    Добавил товары с помощью Vue, сделал метод добавления товара в корзину

---
+++ Homework-1/myScriptVue.js
@@ -0,0 +1,69 @@
+const productApi = 'https://raw.githubusercontent.com/GeekBrainsTutorial/online-store-api/master/responses/';
+const products= [
+  {id: 1, title: 'Телевизор Samsung', price: 2000, img: 'http://placehold.it/350x300', quantity: 1},
+  {id: 2, title: 'Iphone 12', price: 4000, img: 'http://placehold.it/350x300', quantity: 1},
+  {id: 3, title: 'Sony Playstation', price: 3000, img: 'http://placehold.it/350x300', quantity: 1},
+  {id: 4, title: 'MacBook Pro', price: 5200, img: 'http://placehold.it/350x300', quantity: 1},
+];
+
+// Приложение на Vue
+const app = new Vue({
+  el: '#myApp',
+  data: {
+    headerLinks: ['Каталог', 'Акции', 'О нас', 'Контакты'],
+    catalogs: 'catalogData.json',
+    imgProduct: 'http://placehold.it/350x300',
+    imgCartProduct: 'http://placehold.it/50x50',
+    goods: [],
+    filteredGoods: [],
+    cartItems: [],
+    searchLine: '',
+    basketIsActive: false,
+  },
+
+  methods: {
+    // Метод делает запрос на сервер, для получения товаров
+    getJson(url) {
+      return fetch(url)
+        .then(result => result.json())
+        .catch(error => {
+          console.log(error);
+        });
+    },
+    // Метод убавляет количество товара, если количество равно 1
+    // то возвращает return
+    addQuantity(quantity, index) {
+      if(quantity > 1) {
+        this.goods[index].quantity--;
+      }
+    },
+    // Метод добавляет товар в корзину
+    addProductCart(product) {
+      let findElem = this.cartItems.find(elem => elem.id_product === product.id_product);
+      if(findElem) {
+        product.quantity++;
+      } else {
+        let cartGood = Object.assign(product);
+        this.cartItems.push(cartGood);
+      }
+    },
+    // Метод фильтрует товар, когда пользователь ищет необходимый товар
+    filter() {
+      
+    },
+    // Метод считает общую сумму товаров в корзине
+    getTotalSum() {
+      
+    },
+  },
+
+  mounted() {
+    this.getJson(`${productApi + this.catalogs}`)
+        .then(data => {
+          for(let el of data) {
+            let prod = Object.assign({quantity: 1}, el);
+            this.goods.push(prod);
+          }
+        });
+  },
+});
